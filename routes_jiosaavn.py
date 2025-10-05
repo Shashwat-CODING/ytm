836db37e7c09593875615a54446d3db4a39fbb59
Shashwat-CODING <bob17040246@gmail.com>
2025-10-05 19:42:44 +0530

    Update routes_jiosaavn.py

--- routes_jiosaavn.py
+++ routes_jiosaavn.py
@@ -40,7 +40,7 @@ def jiosaavn_search():
     # Construct JioSaavn API URL
     search_query = f"{title} {artist}"
     jiosaavn_api_url = (
-        f"https://fragrant-rain-244a.bob17040246.workers.dev/proxy?url=https://www.jiosaavn.com/api.php"
+        f"https://fragrant-rain-244a.bob17040246.workers.dev/api.php"
         f"?_format=json"
         f"&_marker=0"
         f"&api_version=4"
@@ -149,7 +149,7 @@ def jiosaavn_search_all():
     
     # Construct JioSaavn API URL
     jiosaavn_api_url = (
-        f"https://fragrant-rain-244a.bob17040246.workers.dev/proxy?url=https://www.jiosaavn.com/api.php"
+        f"https://fragrant-rain-244a.bob17040246.workers.dev/api.php"
         f"?_format=json"
         f"&_marker=0"
         f"&api_version=4"
